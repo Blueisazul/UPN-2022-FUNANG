2064b2a34ce96e47a199d6ebce98f0c83fcfaf26
Blueisazul <113708618+Blueisazul@users.noreply.github.com>
2022-10-05 21:10:27 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿char caracter;
+
+do {
+    Console.Write("Ingrese un caracter: ");
+    caracter = char.Parse(Console.ReadLine());
+} while (caracter != 'a' && caracter != 'e' && caracter != 'i' && caracter != 'o' && caracter != 'u'
+
+    caracter != 'A' && caracter != 'E' && caracter != 'I' && caracter != 'O' && caracter != 'U');
+
